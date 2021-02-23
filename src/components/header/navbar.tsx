ae2f3e25f0c1a13e3ce78a295973322c9b8dbc1d
Nguyen Trung <trungdipdo5@gmail.com>
2021-02-24 02:00:30 +0700

    make nav for mobi

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useMedia from "../../hooks/use-media";
 
 export const Category = (props: any) => {
@@ -30,11 +30,62 @@ export const categoriesData = [
 
 export const Navbar = (props: any) => {
   const isWide = useMedia("(min-width: 480px)");
+  const [isOpen, setIsOpen] = useState(false);
   if (!isWide)
     return (
-      <>
-        <span>Categories</span>
-      </>
+      <div>
+        <span
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          Categories
+        </span>
+        <div
+          style={{
+            paddingTop: "calc(var(--vh, 1vh) * 16)",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            background: "white",
+            width: "calc(var(--vw, 1vw) * 80)",
+            height: "calc(var(--vh, 1vh) * 100)",
+            zIndex: -1,
+            display: isOpen ? "" : "none",
+            boxShadow: "calc(var(--vw, 1vw) * 20) 0 rgba(136, 136, 136, 0.329)",
+          }}
+        >
+          <button
+            style={{
+              position: "fixed",
+              top: 70,
+              right: "calc(var(--vw, 1vw) * 23)",
+              padding: "20px",
+              border: "none",
+              background: "transparent",
+              fontSize: "30px",
+            }}
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          >
+            X
+          </button>
+          {categoriesData.map((el, id) => {
+            return (
+              <Category
+                onClick={() => {
+                  setIsOpen(false);
+                  props.setByCategory(el);
+                  window.scrollTo(0, window.innerHeight - 40);
+                }}
+                key={id}
+                title={el}
+              />
+            );
+          })}
+        </div>
+      </div>
     );
   return (
     <>
